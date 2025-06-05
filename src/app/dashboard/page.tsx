c2398aa7373ce42c973fe43be11155eee73192b1
Charlie1JJdh <clementballot803@gmail.com>
2025-06-05 21:35:58 +0300

    com

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,7 +4,7 @@ import { useAuth } from "@/contexts/auth-context";
 import { Button } from "@/components/ui/button";
 import ProtectedRoute from "@/components/protected-route";
 import { AppSidebar } from "@/components/app-sidebar";
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
+import { ChartBarInteractive } from "@/components/chart-bar-interactive";
 import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
@@ -37,9 +37,11 @@ function DashboardContent() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                <ChartBarInteractive />
               </div>
-              <DataTable data={data} />
+              <div className="px-4 lg:px-6">
+                <DataTable data={data} />
+              </div>
             </div>
           </div>
         </div>
